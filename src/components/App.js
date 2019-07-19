ec8add5b475da823eba3bc7fc51e8e9b4ac3d89b
yasuhiro <yasucom4646@gmail.com>
2019-07-18 23:30:07 -0700

    Init

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HomePage from '../pages/homepage/homepage.components';
+
+import '../styles/reset.style.scss';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homepage/homepage.components.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from '../../components/directory.component';
+
+import './hompage.style.scss'
+
+const HomePage = () => {
+    return(
+        <div className="homepage">
+
+        </div>
+    );
+}
+
+export default HomePage;
