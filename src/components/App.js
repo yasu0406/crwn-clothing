7c30784a2880f5662b74ce6394d5f16d798a5097
yasuhiro <yasucom4646@gmail.com>
2019-08-14 18:41:45 -0700

    add checkout-page, checkout-item component and updated cart to use selectors from reselect

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import '../styles/reset.style.scss';
 import '../styles/base.style.scss';
@@ -8,9 +9,11 @@ import '../styles/base.style.scss';
 import HomePage from '../pages/homepage/homepage.component';
 import ShopPage from '../pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from '../pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import ChackoutPage from '../pages/checkout/checkout.component';
 import Header from '../components/header/header.component.jsx';
 import { auth, createUserProfileDocument } from '../firebase/firebase.utils';
 import { setCurrentUser } from '../redux/user/user.actions';
+import { selectCurrentUser } from '../redux/user/user.selectors';
 
 class App extends React.Component {
 
@@ -47,6 +50,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={ChackoutPage} />
           <Route exact path='/signin'
             render = {
               () => 
@@ -63,8 +67,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-    currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector ({
+    currentUser: selectCurrentUser
 })
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
