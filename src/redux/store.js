21e6a05c5f7c128576696c370be1e45cec039830
yasuhiro <yasucom4646@gmail.com>
2019-09-04 20:40:39 -0700

    Deleted redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk'
 import createSagaMiddleware from 'redux-saga';
 
 import rootSaga from './root-saga';
